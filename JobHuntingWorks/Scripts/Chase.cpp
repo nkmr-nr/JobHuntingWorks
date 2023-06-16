497d390875c4c8a8556407208b5b4696fdb4f493
DESKTOP-DEFOH0H\narun <narunaru318@outlook.jp>
2023-06-16 11:30:51 +0900

    Chase視野に入ると追いかけるようにした。

--- JobHuntingWorks/Scripts/Chase.cpp
+++ JobHuntingWorks/Scripts/Chase.cpp
@@ -1,8 +1,23 @@
 
+#include"Wait.h"
 #include "Chase.h"
 #include"Enemy.h"
 
 StateBase* Chase::Update(Enemy* enemy)
 {
+	VECTOR direction = enemy->GetTargetDerectionXZ();
+	const int  speed = 5;
+
+	direction.x *= speed;
+	direction.z *= speed;
+
+	enemy->SetMoveVec(direction);
+
+	enemy->MoveVec(direction);//ˆÚ“®
+
+	if (enemy->IsFound() == false)
+	{
+		return Wait::Instance();//”ÍˆÍ“à‚É‚¢‚È‚¢‚©‚çWait‚É•Ô‚é
+	}
 	return this;
 }
